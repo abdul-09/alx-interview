d024307700153cc30eaf95e7f37300f571b87d07
abdul-09 <abdul.9.hussein@gmail.com>
2024-01-10 09:04:48 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,17 +2,18 @@
 
 from collection import deque
 
+
 def canUnlockAll(boxes):
     num_boxes = len(boxes)  # Total number of boxes
-    unlocked = [False] * num_boxes # Track if each box is unlocked
+    unlocked = [False] * num_boxes  # Track if each box is unlocked
     unlocked[0] = True  # The first box is initially unlock
     queue = deque([0])  # Start BFS from the first box
-    
+
     while queue:
         current_box = queue.popleft()
         for key in boxes[current_box]:
             if key < num_boxes and not unlocked[key]:
                 unlocked[key] = True
                 queue.append(key)
-                
+
     return all(unlocked)
